e06efa9d8008c839a412b6b93a906afaf6de681f
dromard <baptiste74600@hotmail.fr>
2019-05-27 11:53:56 +0200

    "First commit "

---
+++ src/com/ensicaen/dromard/Servlet.java
@@ -0,0 +1,25 @@
+package com.ensicaen.dromard;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Servlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+            response.setContentType("text/html");
+            response.setCharacterEncoding( "UTF-8" );
+            PrintWriter out = response.getWriter();
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<meta charset=\"utf-8\" />");
+            out.println("<title>"+request.getParameter("text")+"</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<p>Ceci est une page générée depuis une servlet.</p>");
+            out.println("</body>");
+            out.println("</html>");
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+    }
+}
