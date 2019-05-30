b633a1aa2f48772532e7ece8e11ce417be8fd4a5
dromard <baptiste74600@hotmail.fr>
2019-05-30 16:58:51 +0200

    refactor

--- src/main/java/Utils/Main.java
+++ src/main/java/Utils/Main.java
@@ -1,7 +1,11 @@
 package Utils;
 
+import org.apache.commons.codec.binary.Hex;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println(Web3Utils.doTransaction("0x8fa7173202d86C746bd884C9f116E356600c6b0E", "ça marche"));
+        String myString = "ça marche";
+        String hexString = Hex.encodeHexString(myString.getBytes());
+        System.out.println(Web3Utils.doTransaction("0x8fa7173202d86C746bd884C9f116E356600c6b0E", hexString));
     }
 }
